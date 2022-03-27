c28aa590a4964ea451f378d388e760dfcc73f77d
Artem Vyazemskiy <artvyz17@yandex.ru>
2022-03-27 13:59:40 +0300

    Add test
    Add XmlTest.marshalAndUnmarshal

---
+++ src/test/java/lmseditor/XmlTest.java
@@ -0,0 +1,74 @@
+package lmseditor;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import lmseditor.question.*;
+import lmseditor.question.component.*;
+
+public class XmlTest {
+
+    @Test
+    public void marshalAndUnmarshal() {
+        List<Answer> answers = new ArrayList<>();
+        answers.add(new Answer("Correct answer 1", 100));
+        answers.add(new Answer("Incorrect answer 1", 0));
+        answers.add(new Answer("Incorrect answer 2", 0));
+
+        QuestionShortAnswer questionShortAnswer = new QuestionShortAnswer("questionShortAnswer-1",
+                new QuestionText("Question short answer 1 text"), answers);
+
+        QuestionChoice questionChoice = new QuestionChoice("questionChoice-1",
+                new QuestionText("Question choice 1 text"),false, answers);
+
+        List<Subquestion> subquestions = new ArrayList<>();
+        subquestions.add(new Subquestion("Subquestion 1 text", "Subquestion 1 answer"));
+        subquestions.add(new Subquestion("Subquestion 2 text", "Subquestion 2 answer "));
+
+        QuestionMatching questionMatching = new QuestionMatching("questionMatching-1",
+                new QuestionText("Question Matching 1 text"), subquestions);
+
+        List<NumericalAnswer> numericalAnswers = new ArrayList<>();
+        numericalAnswers.add(new NumericalAnswer(15.0, 100, 0.0));
+        numericalAnswers.add(new NumericalAnswer(-3.0, 100, 0.0));
+
+        QuestionNumerical questionNumerical = new QuestionNumerical("questionNumerical-1",
+                new QuestionText("Question Numerical 1 text"), numericalAnswers);
+
+        QuestionCollection questions = new QuestionCollection();
+
+        questions.addQuestion(questionShortAnswer);
+        questions.addQuestion(questionChoice);
+        questions.addQuestion(questionMatching);
+        questions.addQuestion(questionNumerical);
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(QuestionCollection.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            StringWriter stringWriter1 = new StringWriter();
+            marshaller.marshal(questions, stringWriter1);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            QuestionCollection questionsUnmarshall = (QuestionCollection) unmarshaller.unmarshal(
+                    new StringReader(stringWriter1.toString()));
+
+            StringWriter stringWriter2 = new StringWriter();
+            marshaller.marshal(questionsUnmarshall, stringWriter2);
+
+            Assertions.assertEquals(stringWriter1.toString(), stringWriter2.toString());
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
